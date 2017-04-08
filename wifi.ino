79f6e2d497e8c13f48fe9b72b45e0cecff467717
griehsler <ronald.griehsler@gmail.com>
2017-04-08 18:58:35 +0200

    assigning HostName before joining network; adapted server mode IP and password

--- wifi.ino
+++ wifi.ino
@@ -19,6 +19,7 @@ bool connectToWifi()
   Serial.print("trying to connect to '" + otherAPSSID + "' ");
 
   WiFi.mode(WIFI_STA);
+  WiFi.hostname(hostName);
   WiFi.begin(otherAPSSID.c_str(), otherAPPassword.c_str());
 
   for (int i = 0; i < 60; i++)
@@ -42,13 +43,13 @@ bool connectToWifi()
 void startAP()
 {
   String ssid = "SwitchIt" + String(ESP.getFlashChipId());
-  IPAddress ip(192, 168, 0, 1);
+  IPAddress ip(192, 168, 1, 1);
   IPAddress subnet(255, 255, 255, 0);
 
   Serial.println("starting WIFI AP");
   WiFi.mode(WIFI_AP);
   WiFi.softAPConfig(ip, ip, subnet);
-  WiFi.softAP(ssid.c_str(), "test1234");
+  WiFi.softAP(ssid.c_str(), "Test1234");
   Serial.println("started WIFI AP '" + ssid + "'");
   Serial.print("IP Address: ");
   Serial.println(WiFi.softAPIP());
